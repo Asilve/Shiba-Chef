a1949add2b0e63f879c9d7ea1527b18c45b18d6b
Anthony Silvester <asilvester196@gmail.com>
2025-02-20 23:53:01 +0000

    Initial commit.
    This is the first commit for ShibaChef. (No code has been written yet.)

---
+++ src/ShibaChef.java
@@ -0,0 +1,12 @@
+/**
+ * ShibaChef is a personal meal planner and shopping list generator.
+ * 
+ * @author Anthony Silvester
+ * @version 0.1.0
+ */
+public class ShibaChef {
+    public static void main(String[] args) {
+
+        
+    }
+}
